ff893880690b4c00646418f9dbdacdd41c89a019
Jakub Zmátlo <115625464+JakubZmatlo@users.noreply.github.com>
2023-09-25 10:11:03 +0200

    Update main.js

--- res/js/main.js
+++ res/js/main.js
@@ -1,7 +1,11 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
 
 let numberOfCookies = 0;
+let costOfClickUpgrade = 20;
+let costOfClickUpgradeIncrease = 1;
+
 
 // {} - scope
 cookie.onclick = () => {
@@ -13,3 +17,35 @@ cookie.onclick = () => {
     console.log(numberOfCookies);
     counter.innerHTML = "Cookies: " + numberOfCookies;
 }
+
+const costOfClickUpgradeFunction = () => {
+    if (numberOfCookies >= costOfClickUpgrade) {
+        //Odecteme cenu upgradu 
+        // pocet susenek a odectu 20
+        numberOfCookies -= costOfClickUpgrade;
+
+        //aktualizace odstavce 
+        counter.innerText = "Cookies: " + numberOfCookies;
+
+        //Zvedneme pocet susenek na kliknuti
+        costOfClickUpgradeIncrease++;
+    }
+};
+
+clickUpgrade.onclick = costOfClickUpgradeFunction
+
+//Vytvoreni funkce 
+//function nazev() {}
+function cheats() {
+    console.log("Cheats on!");
+    numberOfCookies += 1000000
+}
+//Spusteni funkce
+cheats();
+
+const cheatsTwo = () => {
+    numberOfCookies *= 2;
+    counter.innerText = " Cookies: " + numberOfCookies;
+}
+
+cheatsTwo();
